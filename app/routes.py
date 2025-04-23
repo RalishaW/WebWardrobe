6c69a0ce457ea331b5d091154e083418cc6bbda1
ralisha <113558505+RalishaW@users.noreply.github.com>
2025-04-23 14:55:18 +0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,27 @@
 
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, request, session
+
+# Introductory / Landing Page
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# Sign Up
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        # handle form submission
+        return redirect(url_for("login"))
+    return render_template("signup.html")
+
+# Log In
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check user credentials and log in
+        return redirect(url_for("wardrobe"))
+    return render_template("login.html")
 
 @app.route('/wardrobe')
 def wardrobe():
