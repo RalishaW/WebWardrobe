36f7e1ede10356d99e0ecbea8578fb39e7c9526f
Premithy <prempatel92004@gmail.com>
2025-05-17 13:38:34 +0800

    Fixed some bugs

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_mail import Mail
-
+from dotenv import load_dotenv
+load_dotenv()
 from .config import Config, config_dict
 
 # Extension instances
